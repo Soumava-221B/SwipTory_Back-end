9f544915bcfc3cb7404d7572fd4e05b1f0a246c1
Soumava-221B <77767745+Soumava-221B@users.noreply.github.com>
2024-04-29 04:50:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "https://swip-tory-front-end.netlify.app/",
+      "https://main--swip-tory-front-end.netlify.app/",
       "http://localhost:3000",
     ],
     credentials: true,
